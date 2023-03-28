7877e4e5b8852347c13c36d0c437a0157f46b0f8
Zhusup Akylbek <78166217+Z-husup@users.noreply.github.com>
2023-03-28 19:33:50 +0600

    Update JOIN_12lab.sql

--- JOIN_12lab.sql
+++ JOIN_12lab.sql
@@ -2,15 +2,6 @@
 USE bd_lab
 
 
-
-SELECT o.Id, o.Date, o.Total, c.FirstName, c.LastName, c.Email
-FROM Orders o
-INNER JOIN Customers c ON o.CustomerId = c.Id;
-
-
-
-
-SELECT o.Id, o.Date, o.Total, c.FirstName, c.LastName, l.Name, l.Price
-FROM Orders o
-INNER JOIN Customers c ON o.CustomerId = c.Id
-INNER JOIN Laptops l ON o.Id = l.OrderId;
+SELECT *
+FROM Customers
+INNER JOIN Orders ON Customers.Id = Orders.CustomerId;
